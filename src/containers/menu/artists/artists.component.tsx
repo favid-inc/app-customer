6cb3f65f4a841b9699e751c47aa3755f4b497294
Gabriel Umbelino <gabriel.f.umbelino@gmail.com>
2019-07-12 17:13:31 -0300

    change artists listener to go to artistDetail

--- src/containers/menu/artists/artists.component.tsx
+++ src/containers/menu/artists/artists.component.tsx
@@ -5,21 +5,16 @@ import { Artist } from '@src/core/model';
 
 interface ComponentProps {
 	artists: Artist[];
-	onTrainingDetails: (index: number) => void;
+	onTrainingDetails: (artistId: string) => void;
 }
 
 export type ArtistsComponentProps = ThemedComponentProps & ComponentProps;
 
 class ArtistsComponent extends React.Component<ArtistsComponentProps> {
-	private onTrainingDetails = (index: number): void => {
-		// tslint:disable-next-line:no-console
-		console.log('[artist.component.tsx] onTraningDetails() =>  index: ' + index);
-	};
-
 	public render(): React.ReactNode {
 		const { artists } = this.props;
 		// console.log(JSON.stringify(artists));
-		return <Easy1 artists={artists} onTrainingDetails={this.onTrainingDetails} />;
+		return <Easy1 artists={artists} onTrainingDetails={this.props.onTrainingDetails} />;
 	}
 }
 
--- src/containers/menu/artists/artists.container.tsx
+++ src/containers/menu/artists/artists.container.tsx
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { NavigationScreenProps } from 'react-navigation';
+
+import * as actions from '../../../store/actions';
+
 import { Artists } from './artists.component';
 import { Artist } from '@src/core/model/artist.model';
-// import database from '@favid-inc/core';
 import { artists } from '@src/core/data/artists';
-
+// import database from '@favid-inc/core';
 // tslint:disable-next-line:no-console
 // console.log(database);
 
@@ -12,16 +15,34 @@ interface State {
 	selectedLevelIndex: number;
 	artists: Artist[];
 }
+interface ArtistContainerProps {
+	onSetArtist: (artist: Artist) => void;
+}
 
-export class ArtistsContainer extends React.Component<NavigationScreenProps, State> {
+type props = NavigationScreenProps & ArtistContainerProps;
+class ArtistsContainer extends Component<props, State> {
 	public state: State = {
 		selectedLevelIndex: 0,
 		artists: artists,
 	};
+	private navigationKey: string = 'SocialContainer';
 
-	private onTrainingDetails = (index: number): void => {};
+	private onTrainingDetails = (artistId: string): void => {
+		const [ artist ] = this.state.artists.filter((a: Artist) => a.id === artistId);
+		this.props.onSetArtist(artist);
+		this.props.navigation.navigate({
+			key: this.navigationKey,
+			routeName: 'Artist Details',
+		});
+	};
 
 	public render(): React.ReactNode {
 		return <Artists artists={this.state.artists} onTrainingDetails={this.onTrainingDetails} />;
 	}
 }
+
+const mapDispatchToProps = (dispatch) => ({
+	onSetArtist: (artist) => dispatch(actions.setArtist(artist)),
+});
+
+export default connect(null, mapDispatchToProps)(ArtistsContainer);
