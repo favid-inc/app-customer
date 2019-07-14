9253c217d21667a210a8aaf429395c803b724638
lmarqs <marques.analista@gmail.com>
2019-07-14 14:51:53 -0300

    add client id for dev

--- src/core/config/config.dev.ts
+++ src/core/config/config.dev.ts
@@ -10,8 +10,7 @@ export const firebase = {
 
 export const auth = {
   behavior: 'web',
-  // androidClientId: '983263249595-p0tldp1tcmk4pmbuj6p42bvto8us7k4i.apps.googleusercontent.com',
-  clientId: '983263249595-2tclp7pj3b23h05l08v8v7af6mgbud1s.apps.googleusercontent.com',
+  clientId: '983263249595-anadgjdnado7mgh9o8eebpbeg4ki5c6m.apps.googleusercontent.com',
   issuer: 'https://accounts.google.com',
   scopes: ['profile', 'email'],
 };
