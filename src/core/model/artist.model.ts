95f70272bb226d214d53f6781a8711dd3e913ab6
Gabriel Umbelino <gabriel.f.umbelino@gmail.com>
2019-07-15 15:42:37 -0300

    create CategoryOfArtistModel interface

--- src/core/model/artist.model.ts
+++ src/core/model/artist.model.ts
@@ -1,1 +1,6 @@
+import { ArtistModel as Artist } from '@favid-inc/api';
 export { ArtistModel as Artist } from '@favid-inc/api';
+export interface CategoryOfArtistModel {
+  key: string;
+  artists: Artist[];
+}
