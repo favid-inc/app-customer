c28a6a9f4f0ec06565a882c6e50008ba62191301
Gabriel Umbelino <gabriel.f.umbelino@gmail.com>
2019-07-17 14:55:34 -0300

    Save customer in database at first time signIn

--- src/store/actions/AuthActions.tsx
+++ src/store/actions/AuthActions.tsx
@@ -20,6 +20,44 @@ export const loadAuthState = () => {
   };
 };
 
+export const verifyCustomer = (customer: CustomerModel) => {
+  return async dispatch => {
+    console.log('[AuthActions.tsx] verifyCustomer() => started');
+
+    const queryParams = `?orderBy="uid"&equalTo="${customer.uid}"`;
+    const response = await fetch(`${config.firebase.databaseURL}/customer.json${queryParams}`);
+    const data: any = await response.json();
+
+    if (Object.keys(data).length) {
+      const customerID = Object.keys(data)[0];
+      if (data[customerID].uid) {
+        console.log('[AuthActions.tsx] verifyCustomer() => finished: user already registred');
+        return;
+      }
+      console.log('[AuthActions.tsx] verifyCustomer() => finished: register user');
+      dispatch(registerCustomer(customer));
+    } else {
+      console.log('[AuthActions.tsx] verifyCustomer() => finished: register user');
+      dispatch(registerCustomer(customer));
+    }
+  };
+};
+
+const registerCustomer = (customer: CustomerModel) => {
+  return async () => {
+    console.log('[AuthActions.tsx] registerCustomer() => started');
+    await fetch(`${config.firebase.databaseURL}/customer.json`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(customer),
+    });
+    console.log('[AuthActions.tsx] registerCustomer() => finished');
+  };
+};
+
 export const signOut = () => {
   AsyncStorage.removeItem(storageKey);
   return {
