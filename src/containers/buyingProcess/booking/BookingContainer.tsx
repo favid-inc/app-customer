83c3167017b19e1b5f71d3580a33bcae4ec38d23
Gabriel Umbelino <gabriel.f.umbelino@gmail.com>
2019-07-28 23:41:31 -0300

    BookingContainer
    add error handling

--- src/containers/buyingProcess/booking/BookingContainer.tsx
+++ src/containers/buyingProcess/booking/BookingContainer.tsx
@@ -2,42 +2,70 @@ import React, { Component } from 'react';
 import { Booking } from './Booking';
 import { connect } from 'react-redux';
 import * as actions from '../../../store/actions';
+import { OrderModel } from '@favid-inc/api';
+import { Alert } from 'react-native';
 
-interface ComponentProps  {
+interface ComponentProps {
   loading: boolean;
   artistId: string;
   price: string;
   userId: number;
-  onPostOrder: (any) => void;
+  idToken: string;
+  error: any;
+  onPostOrder: (order: OrderModel, idToken: string) => void;
+  onSignOut: () => void;
+  onError: (error: any) => void;
 }
-
-
 
 class BookingContainer extends Component<ComponentProps> {
   private onSend(orderInstructions) {
-    const order = {
+    const order: OrderModel = {
       ...orderInstructions,
-      price: this.props.price,
       artistId: this.props.artistId,
       userId: this.props.userId,
     };
-    this.props.onPostOrder(order);
+    this.props.onPostOrder(order, this.props.idToken);
+  }
+  public shouldComponentUpdate(nextProps) {
+    if (this.props.error !== nextProps || this.props.loading !== this.props.loading) {
+      return true;
+    }
+    return false;
   }
 
   public render(): React.ReactNode {
-    return <Booking onSend={(order) => this.onSend(order)} loading={this.props.loading}/>;
+    if (this.props.error) {
+      const action =
+        this.props.error.status === 403
+          ? [
+              {
+                text: 'Ok, efetuar login novamente.',
+                onPress: () => this.props.onSignOut(),
+              },
+            ]
+          : null;
+      Alert.alert(this.props.error.message, null, action, { cancelable: false });
+      this.props.onError(null);
+    }
+    return <Booking onSend={(order: OrderModel) => this.onSend(order)} loading={this.props.loading} />;
   }
 }
 
-const mapStateToProps = ({order, artist, auth}) => ({
+const mapStateToProps = ({ order, artist, auth }) => ({
   loading: order.loading,
   artistId: artist.artist.id,
-  price: artist.artist.price,
   userId: auth.authState.uid,
+  idToken: auth.authState.idToken,
+  error: order.error,
 });
 
 const mapDispatchToProps = dispatch => ({
-  onPostOrder: order => dispatch(actions.postOrder(order)),
+  onError: error => dispatch(actions.orderError(error)),
+  onPostOrder: (order: OrderModel, idToken: string) => dispatch(actions.postOrder(order, idToken)),
+  onSignOut: () => dispatch(actions.signOut()),
 });
 
-export default connect(mapStateToProps, mapDispatchToProps)(BookingContainer);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(BookingContainer);
