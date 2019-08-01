80418906cd953e167c759d3566a2cfc820847c99
lmarqs <marques.analista@gmail.com>
2019-08-01 20:25:28 -0300

    fix casee sensitive import

--- src/containers/orders/navigators.tsx
+++ src/containers/orders/navigators.tsx
@@ -1,6 +1,6 @@
 import { NavigationContainer, createStackNavigator, NavigationRouteConfigMap } from 'react-navigation';
-import { OrdersContainer } from './orders';
-import { OrderDetailsContainer } from './orderDetails';
+import { OrdersContainer } from './Orders';
+import { OrderDetailsContainer } from './OrderDetails';
 import { TopBarNavigationOptions } from '@src/core/navigation/options';
 
 export const OrderDetailsContainerNavigationMap: NavigationRouteConfigMap = {
