1bef9c0d91a8c490c3e42565f82d024e0328519b
lmarqs <marques.analista@gmail.com>
2019-07-31 22:20:04 -0300

    remove mapping

--- src/store/actions/OrderActions.tsx
+++ src/store/actions/OrderActions.tsx
@@ -5,6 +5,7 @@ import { OrderModel, OrderFlow, OrderStatus } from '@favid-inc/api';
 export const postOrder = (order: OrderModel, idToken: String) => {
   return async dispatch => {
     dispatch(loadOrderStarted());
+
     const response = await fetch(`${config.api.baseURL}/${OrderFlow.PLACE}`, {
       method: 'POST',
       headers: {
@@ -15,6 +16,7 @@ export const postOrder = (order: OrderModel, idToken: String) => {
 
       body: JSON.stringify(order),
     });
+
     if (!response.ok) {
       const message = response.status === 403 ? 'Sua sessão expirou.' : 'Erro interno do servidor.';
       dispatch(orderError({ status: response.status, message }));
@@ -46,7 +48,7 @@ export const getOrders = (userId: string) => {
     const response = await fetch(`${config.firebase.databaseURL}/order.json${queryParams}`);
     if (response.status === 200) {
       const data = await response.json();
-      const orders: OrderModel[] = Object.keys(data).map(id => ({ id, ...data[id] }));
+      const orders: OrderModel[] = Object.values(data);
       dispatch(storeOrders(orders));
     } else {
       dispatch(orderError({ status: response.status, message: 'Erro ao listar pedidos.' }));
