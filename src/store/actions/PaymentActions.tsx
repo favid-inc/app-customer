81cef860dcef1072fd3e9a7d1f7ca80659173588
Gabriel Umbelino <gabriel.f.umbelino@gmail.com>
2019-08-10 02:33:12 -0300

    update payment reducer just to store paymentToken

--- src/store/actions/PaymentActions.tsx
+++ src/store/actions/PaymentActions.tsx
@@ -1,27 +1,5 @@
-import { START_PROCESSING_PAYMENT, STOP_PROCESSING_PAYMENT } from './ActionTypes';
-
-export const onSendPayment = payment => {
-  return async dispatch => {
-    dispatch(onStartProcessingPayment());
-    const token = '';
-    // get token payment
-    Iugu.setAccountID('77F674C400DF469B80B18B0D00DB9C1D');
-    Iugu.createPaymentToken(this, response => {
-      if (response.errors) {
-        console.log('Erro salvando cartão');
-      } else {
-        console.log('Token criado:' + token);
-      }
-    });
-
-    dispatch(onStopProcessingPayment());
-  };
-};
-
-export const onStartProcessingPayment = () => ({
-  type: START_PROCESSING_PAYMENT,
-});
-
-export const onStopProcessingPayment = () => ({
-  type: STOP_PROCESSING_PAYMENT,
+import { SET_PAYMENT_TOKEN } from './ActionTypes';
+export const setPaymentToken = (paymentToken: string) => ({
+  type: SET_PAYMENT_TOKEN,
+  paymentToken,
 });
