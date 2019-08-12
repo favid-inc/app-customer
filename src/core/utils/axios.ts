8a9fcfff6d7fd132bb00a7c0a2bfcf5a3a923a0d
Gabriel Umbelino <gabriel.f.umbelino@gmail.com>
2019-08-11 23:24:56 -0300

    add axios default headers

--- src/core/utils/axios.ts
+++ src/core/utils/axios.ts
@@ -1,20 +1,23 @@
 import axios from 'axios';
 import { api } from '../config';
 
+axios.defaults.baseURL = api.baseURL;
+axios.defaults.headers = {
+  Accept: 'application/json',
+  'Content-Type': 'application/json',
+};
+
+axios.interceptors.request.use(config => {
+  console.log('[Axios.ts] reqInterceptor() config.headers: ', config.headers);
+  console.log('[Axios.ts] reqInterceptor() config.data: ', config.data);
+  return config;
+});
+
 export const axiosInstance = idToken => {
-  const reqInterceptor = axios.interceptors.request.use(config => {
-    config.headers = {
-      ...config.headers,
-      Accept: 'application/json',
-      'Content-Type': 'application/json',
-      Authorization: `Bearer ${idToken}`,
-    };
-
-    console.log('[Axios.ts] reqInterceptor() config: ', config);
-    return config;
-  });
-
-  axios.interceptors.request.eject(reqInterceptor);
   axios.defaults.baseURL = api.baseURL;
+  axios.defaults.headers = {
+    ...axios.defaults.headers,
+    Authorization: `Bearer ${idToken}`,
+  };
   return axios;
 };
