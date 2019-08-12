aaf23ef73628d2fa684c7ba4c101fd6be0e59888
Gabriel Umbelino <gabriel.f.umbelino@gmail.com>
2019-08-11 23:22:21 -0300

    remove post order from redux

--- src/containers/buyingProcess/booking/navigator.ts
+++ src/containers/buyingProcess/booking/navigator.ts
@@ -1,10 +1,10 @@
-import { NavigationContainer, createStackNavigator } from 'react-navigation';
+import { NavigationRouteConfigMap } from 'react-navigation';
 import { BookingContainer } from './BookingContainer';
 import { TopBarNavigationOptions } from '@src/core/navigation/options';
 
 export const BookingNavigation = {
   Pedido: {
     screen: BookingContainer,
-    navigationOptions: TopBarNavigationOptions,
+    defaultNavigationOptions: TopBarNavigationOptions,
   },
 };
--- src/store/actions/index.ts
+++ src/store/actions/index.ts
@@ -1,4 +1,4 @@
 export { auth, loadAuthState, signOut, verifySession } from './AuthActions';
 export { setArtist, getArtist, removeArtist, listArtists } from './ArtistActions';
-export { postOrder, getOrders, setOrder, orderError } from './OrderActions';
+export { getOrders, setOrder, orderError } from './OrderActions';
 export { setPaymentToken } from './PaymentActions';
