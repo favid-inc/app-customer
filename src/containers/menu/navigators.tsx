32356a64a5e1cdfae4df2854ae9b7048504a4332
Gabriel Umbelino <gabriel.f.umbelino@gmail.com>
2019-08-13 23:00:08 -0300

    connect temporary navigator to context

--- src/containers/menu/navigators.tsx
+++ src/containers/menu/navigators.tsx
@@ -7,10 +7,24 @@ import {
 import { MenuContainer } from './menu.container';
 import { ArtistsNavigator } from '../artists';
 import { OrdersNavigator } from '../orders';
+import { OrderInfoContainer } from '../buyingProcess/orderInfo/OrderInfoContainer';
 import { SettingsNavigator } from '../settings';
+import { connect } from '../buyingProcess/context';
+export const OrderInfoNavigator = connect(
+  createStackNavigator(
+    {
+      OrderInfoContainer,
+    },
+    {
+      headerMode: 'screen',
+      defaultNavigationOptions: { header: null },
+    },
+  ),
+);
 
 const menuNavigationMap: NavigationRouteConfigMap = {
-  Artists: ArtistsNavigator,
+  OrderInfo: OrderInfoNavigator,
+  // Artists: ArtistsNavigator,
   Orders: OrdersNavigator,
   Settings: SettingsNavigator,
 };
