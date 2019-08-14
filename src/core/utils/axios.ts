54022553bfbb9c79800d7a0cef520ca1a57ba140
Gabriel Umbelino <gabriel.f.umbelino@gmail.com>
2019-08-13 23:01:08 -0300

    remove unecessary axios config

--- src/core/utils/axios.ts
+++ src/core/utils/axios.ts
@@ -3,15 +3,8 @@ import { api } from '../config';
 
 axios.defaults.baseURL = api.baseURL;
 axios.defaults.headers = {
-  Accept: 'application/json',
   'Content-Type': 'application/json',
 };
-
-axios.interceptors.request.use(config => {
-  console.log('[Axios.ts] reqInterceptor() config.headers: ', config.headers);
-  console.log('[Axios.ts] reqInterceptor() config.data: ', config.data);
-  return config;
-});
 
 export const axiosInstance = idToken => {
   axios.defaults.baseURL = api.baseURL;
