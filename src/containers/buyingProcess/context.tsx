16f876465d4a4e02cf548bcb75b43574779c57d8
Gabriel Umbelino <gabriel.f.umbelino@gmail.com>
2019-08-15 22:06:52 -0300

    add buyingprocess context improvements

--- src/containers/buyingProcess/context.tsx
+++ src/containers/buyingProcess/context.tsx
@@ -3,7 +3,7 @@ import { NavigationScreenProps } from 'react-navigation';
 import { NavigationContainer } from 'react-navigation';
 import { OrderModel } from '@favid-inc/api';
 import { PaymentResponse, ChargeResponse, Customer } from '@src/core/model';
-
+import { connect as reduxConnect } from 'react-redux';
 export interface IBuyingProcessContext {
   order?: OrderModel;
   chargeData?: ChargeResponse;
@@ -14,10 +14,17 @@ export interface IBuyingProcessContext {
   setChargeData?: (chargeData: ChargeResponse) => void;
   setCustomer?: (customer: Customer) => void;
 }
+interface OrderInfoContainerProps {
+  idToken: string;
+  customer: Customer;
+}
 
 export const BuyingProcessContext = React.createContext<IBuyingProcessContext>({});
 export function connect(Navigator: NavigationContainer) {
-  class ContextNavigator extends React.Component<NavigationScreenProps, IBuyingProcessContext> {
+  class ContextNavigator extends React.Component<
+    NavigationScreenProps & OrderInfoContainerProps,
+    IBuyingProcessContext
+  > {
     public state: IBuyingProcessContext = {
       order: {},
       chargeData: {},
@@ -33,14 +40,22 @@ export function connect(Navigator: NavigationContainer) {
     static screenProps = Navigator.screenProps;
     static navigationOptions = Navigator.navigationOptions;
 
+    public componentWillMount() {
+      this.state.setCustomer(this.props.customer);
+    }
+
     public render() {
       return (
         <BuyingProcessContext.Provider value={this.state}>
-          <Navigator {...this.props} />
+          <BuyingProcessContext.Consumer>{() => <Navigator {...this.props} />}</BuyingProcessContext.Consumer>
         </BuyingProcessContext.Provider>
       );
     }
   }
+  const mapStateToProps = ({ auth }) => ({
+    idToken: auth.authState.idToken,
+    customer: auth.customer,
+  });
 
-  return ContextNavigator;
+  return reduxConnect(mapStateToProps)(ContextNavigator);
 }
