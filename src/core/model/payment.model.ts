5d5b9ed91e0ccdf1a72d14e7868972a99aa8d7d4
Gabriel Umbelino <gabriel.f.umbelino@gmail.com>
2019-08-18 15:54:30 -0300

    adjust paymentModel

--- src/core/model/payment.model.ts
+++ src/core/model/payment.model.ts
@@ -1,10 +1,12 @@
+import {Order} from '@favid-inc/api';
+
 export interface CreditCard {
-  number: string;
-  verification_value: string;
-  first_name: string;
-  last_name: string;
-  month: string;
-  year: string;
+  number?: string;
+  verification_value?: string;
+  first_name?: string;
+  last_name?: string;
+  month?: string;
+  year?: string;
 }
 
 export interface Payment {
@@ -39,37 +41,35 @@ export interface Item {
   price_cents: number;
 }
 
-export interface Charge {
-  method: string;
-  token: string;
-  customer_payment_method_id: string;
-  restrict_payment_method: boolean;
-  customer_id: string;
-  invoice_id: string;
-  email: string;
-  months: number;
-  discount_cents: number;
-  bank_slip_extra_days: number;
-  keep_dunning: boolean;
-  items: Item[];
-  payer: Payer;
-  order_id: string;
-}
 
-export interface PaymentResponse {
-  id?: string;
+export interface PaymentToken {
+  account_id?: string;
   method?: string;
-  extra_info?: {
-    brand?: string;
-    holder_name?: string;
-    display_number?: string;
-    bin?: string;
-    month?: number;
-    year?: number;
-  };
-  test?: boolean;
-}
+  test?: string;
+  data?: CreditCard;
+};
 
+export interface DirectCharge {
+  method?: string;
+  token?: string;
+  customer_payment_method_id?: string;
+  restrict_payment_method?: string;
+  customer_id?: string;
+  invoice_id?: string;
+  email?: string;
+  months?: string;
+  discount_cents?: string;
+  bank_slip_extra_days?: number;
+  keep_dunning?: boolean;
+  items?: Item[];
+  payer?: Payer;
+  order_id?: string;
+}
+export interface Charge {
+  order: Order;
+  paymentToken?: PaymentToken;
+  directCharge?: DirectCharge;
+}
 export interface ChargeResponse {
   message?: string;
   errors?: object;
