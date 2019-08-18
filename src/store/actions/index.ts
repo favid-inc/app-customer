1071ced0d25302c828c64dcc4aed56c4c97c8ec3
Gabriel Umbelino <gabriel.f.umbelino@gmail.com>
2019-08-18 15:54:11 -0300

    remove paymentActions from store

--- src/store/actions/index.ts
+++ src/store/actions/index.ts
@@ -1,4 +1,4 @@
 export { auth, loadAuthState, signOut, verifySession } from './AuthActions';
 export { setArtist, getArtist, removeArtist, listArtists } from './ArtistActions';
 export { getOrders, setOrder, orderError } from './OrderActions';
-export { setPaymentToken } from './PaymentActions';
+// export { setPaymentToken } from './PaymentActions';
