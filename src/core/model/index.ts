c9653d5ae63bd7e8aef74e00f2e0729e4aa5ef8e
Gabriel Umbelino <gabriel.f.umbelino@gmail.com>
2019-08-24 02:10:11 -0300

    add temporary artistRate interface

--- src/core/model/index.ts
+++ src/core/model/index.ts
@@ -1,1 +1,7 @@
 export { Profile, ProfileSocials, ProfileActivity } from './profile.model';
+
+export interface ArtistRate {
+  rate: 0 | 1 | 2 | 3 | 4 | 5;
+  message: string;
+  customerName: string;
+}
