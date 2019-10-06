fcde816b104b8226bc86575563c26a9826d5ea0b
lmarqs <marques.analista@gmail.com>
2019-10-06 12:32:08 -0300

    update prod keys

--- src/core/config/dev.env.ts
+++ src/core/config/dev.env.ts
@@ -14,7 +14,6 @@ export const api = {
 
 export const auth = {
   google: {
-    behavior: 'web',
     clientId: '983263249595-2tclp7pj3b23h05l08v8v7af6mgbud1s.apps.googleusercontent.com',
     issuer: 'https://accounts.google.com',
     scopes: ['profile', 'email'],
