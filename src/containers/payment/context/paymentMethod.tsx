23727b806f0f286dcb9bd9b0c22080cd9dabb56f
lmarqs <marques.analista@gmail.com>
2019-12-05 21:13:33 -0300

    set credit_card as default payment method

--- src/containers/payment/context/paymentMethod.tsx
+++ src/containers/payment/context/paymentMethod.tsx
@@ -20,7 +20,7 @@ export const PaymentMethodContext = React.createContext<State>(null);
 
 export class PaymentMethodContextProvider extends React.Component<Props, State> {
   public state: State = {
-    payment_method: 'boleto',
+    payment_method: 'credit_card',
     setCardCvv: (card_cvv) => this.setState({ card_cvv }),
     setCardExpirationDate: (card_expiration_date) => this.setState({ card_expiration_date }),
     setCardHash: (card_hash) => this.setState({ card_hash }),
