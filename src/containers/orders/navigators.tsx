c500ac8db99dc850927e6b95058c374571d6e712
lmarqs <marques.analista@gmail.com>
2020-01-06 18:15:57 -0300

    rename dir

--- src/containers/orders/navigators.tsx
+++ src/containers/orders/navigators.tsx
@@ -1,7 +1,7 @@
 import { createStackNavigator } from 'react-navigation';
 import { PaymentNavigator } from '@src/containers/payment';
 import { connect } from '@src/containers/payment/context';
-import { OrdersContainer } from './orders';
+import { OrdersContainer } from './list';
 
 export const OrdersNavigator = connect(
   createStackNavigator(
