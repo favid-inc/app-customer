3e7d2dfd9dfdaccf12ae8e05f35a325c58db681a
lmarqs <marques.analista@gmail.com>
2020-01-06 18:08:45 -0300

    create cancel order button

---
+++ src/containers/orders/orders/orderCard/CancelOrderButton.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Button } from '@kitten/ui';
+
+export const CancelOrderButton: React.FunctionComponent<null> = () => {
+  return (
+    <Button size='medium'>
+      Cancelar Pedido
+    </Button>
+  );
+};
