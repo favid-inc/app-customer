988ad4e279d5c397da5c4e63a41d0d0c97030dbc
lmarqs <marques.analista@gmail.com>
2020-01-10 21:26:06 -0300

    add alert on booking

--- src/containers/artistDetails/booking/BookingContainer.tsx
+++ src/containers/artistDetails/booking/BookingContainer.tsx
@@ -23,6 +23,15 @@ export class BookingContainer extends Component<Props, State> {
   };
 
   public componentDidMount() {
+    Alert.alert(
+      'Atenção',
+      `
+      O objetivo da FAVID é que todos se divirtam com as mensagens personalizadas.
+      Para que a celebridade aceite seu pedido, escreva um texto legal de forma que ela queira gravar. Evite conteúdos de cunho racista, homofóbico ou que façam apologia à violência ou ao uso de drogas.
+      Lembre-se que a celebridade irá escolher se aceitará ou não o seu pedido! ;-)
+      `,
+    );
+
     const { navigation } = this.props;
     const artist = navigation.getParam('artist');
     this.setState({ artist });
